7f07c39dcc48b80893c0e6123639a8be67efda46
Dawson Whitehead <dwahme1@gmail.com>
2019-04-30 23:10:59 -0500

    Added initial structure

---
+++ massey/model.py
@@ -0,0 +1,8 @@
+
+class Model:
+
+    def __init__(self, groups):
+        self.groups = groups
+
+    def create_neighborhoods(self, number, percent_poor):
+        pass
---
+++ massey/neighborhood.py
@@ -0,0 +1,14 @@
+
+class City:
+
+    def __init__(self, groups, neighborhoods, dissimilarity_index):
+        self.groups = groups
+        self.neighborhoods = neighborhoods
+        self.dissimilarity_index = dissimilarity_index
+
+        self.matrix = self.generate()
+
+    def generate(self):
+        matrix = []
+
+        return matrix
