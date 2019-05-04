c1fbe3c1b8ee57c5503e3f4473cf2a5ffb5b01f5
Dawson Whitehead <dwahme1@gmail.com>
2019-05-04 14:36:24 -0500

    Added functions to generate segregated and uniform cities

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import massey.neighborhood as nbhd
 
 if __name__ == "__main__":
-    pass
+    groups0 = [("humans", 20), ("elves", 5)]
+    groups1 = [("humans", 18), ("elves", 6), ("orcs", 12)]
+
+    city0 = nbhd.City(groups0, 5)
+
+    print(city0.generate_uniform())
+    print(city0.generate_segregated())
+
+    city1 = nbhd.City(groups1, 6)
+
+    print(city1.generate_uniform())
+    print(city1.generate_segregated())
+
--- massey/neighborhood.py
+++ massey/neighborhood.py
@@ -1,14 +1,50 @@
 
 class City:
 
-    def __init__(self, groups, neighborhoods, dissimilarity_index):
+    def __init__(self, groups, neighborhoods):
         self.groups = groups
         self.neighborhoods = neighborhoods
-        self.dissimilarity_index = dissimilarity_index
 
-        self.matrix = self.generate()
+        self.matrix = []
 
-    def generate(self):
+    # Evenly evenly distributes people across a given number of neighborhoods
+    def generate_uniform(self):
         matrix = []
 
+        # Build each neighborhood one at a time
+        for _ in range(self.neighborhoods):
+            neighborhood = []
+
+            # Get an even distribution from each group
+            for group in self.groups:
+                neighborhood.append((group[0], group[1] / self.neighborhoods))
+
+            matrix.append(neighborhood)
+
+        self.matrix = matrix
+
         return matrix
+
+    def generate_segregated(self):
+        matrix = []
+
+        # Determine the total number of people
+        # then figure out how many neighborhoods each group gets
+        num_people = sum([num for (name, num) in self.groups])
+        neighborhood_nums = [self.neighborhoods * num // num_people 
+            for (name, num) in self.groups]
+
+        print(neighborhood_nums)
+
+        # Distribute according to how many neighborhoods the group gets
+        for (num, group) in zip(neighborhood_nums, self.groups):
+
+            for _ in range(num):
+                matrix.append([(group[0], group[1] / num)])
+
+        self.matrix = matrix
+
+        return matrix
+
+
+
