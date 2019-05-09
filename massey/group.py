96cfa5d4045a081dbc4ec755578e632c2347a1b9
Dawson Whitehead <dwahme1@gmail.com>
2019-05-09 00:01:15 -0500

    Added toString methods

--- massey/group.py
+++ massey/group.py
@@ -1,3 +1,4 @@
+import random
 
 class Group:
 
@@ -5,6 +6,12 @@ class Group:
         self.name = name
         self.number = number
         self.poverty_level = poverty_level
+
+    def __str__(self):
+        return "{}:{}:{}".format(self.name, self.number, self.poverty_level)
+
+    def __repr__(self):
+        return self.__str__()
 
     def fill_nbhd(self, inv_prop):
         return (self.name, self.number // inv_prop, self.poverty_level)
@@ -14,3 +21,7 @@ class Group:
         poor = Group(self.name + "_poor", self.number // 2, 
                      self.poverty_level * 2)
         return rich, poor
+
+    def scatter(self, x_off, y_off):
+        return [(random.uniform(0, 1) + x_off, random.uniform(0, 1) + y_off) 
+                for _ in range(self.number)]
