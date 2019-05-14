5a3e2024e988a434e98f9efefbb8dde018fdded9
Dawson Whitehead <dwahme1@gmail.com>
2019-05-14 13:36:54 -0500

    Added skeleton for dissimilarity indexes

---
+++ massey/analyzer.py
@@ -0,0 +1,13 @@
+from massey import group
+from massey import city
+
+# https://www2.census.gov/programs-surveys/demo/about/housing-patterns/multigroup_entropy.pdf
+# Pages 6, 7
+def entropy_score(city, group):
+    pass
+
+# Calculates an entropy index of a city
+# https://www2.census.gov/programs-surveys/demo/about/housing-patterns/multigroup_entropy.pdf
+# Page 7
+def entropy_index(city):
+    pass
