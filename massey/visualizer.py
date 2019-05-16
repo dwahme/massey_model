7e61677c28c2ea21ec42c32b06ec81fa97962a00
Dawson Whitehead <dwahme1@gmail.com>
2019-05-16 18:57:27 -0500

    Added better visualizations

--- massey/visualizer.py
+++ massey/visualizer.py
@@ -22,7 +22,9 @@ def plot(city, save=None):
 
     xmax, ymax = factor_int(city.num_nbhds)
 
+    choices = "bgrcmykw"
     colors = {}
+    names_used = []
 
     for x in range(xmax):
         for y in range(ymax):
@@ -30,22 +32,32 @@ def plot(city, save=None):
 
             for grp in groups:
 
-                if grp.name in colors.keys():
-                    color = colors[grp.name]
-                    label = False
+                stripped_name = grp.name.split("_", 1)[0]
+
+                if stripped_name in colors.keys():
+                    color = colors[stripped_name]
                 else:
-                    color = (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
-                    colors[grp.name] = color
+                    color = choices[0]
+                    choices = choices[1:]
+                    colors[stripped_name] = color
+                
+                label = False
+                if grp.name not in names_used:
+                    names_used.append(grp.name)
                     label = True
+
+                marker = "^"
+                if "_poor" in grp.name:
+                    marker = "."
 
                 scattered = grp.scatter(x, y)
                 xs = [x for x, y in scattered]
                 ys = [y for x, y in scattered]
 
                 if label == True:
-                    plt.scatter(xs, ys, c=[color], label=grp.name)
+                    plt.scatter(xs, ys, marker=marker, c=color, label=grp.name)
                 else:
-                    plt.scatter(xs, ys, c=[color])
+                    plt.scatter(xs, ys, marker=marker, c=color)
 
     plt.legend(loc="center right", framealpha=1)
 
